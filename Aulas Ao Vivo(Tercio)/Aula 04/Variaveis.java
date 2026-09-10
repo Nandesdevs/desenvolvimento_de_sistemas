09edb025a5bfc434a81d74f85498b441da37b90f
Nandes_ <hernandessantos102@gmail.com>
2026-09-10 08:42:01 -0300

    Casting, variaveis e tipos primitivos.

---
+++ Aulas Ao Vivo(Tercio)/Aula 04/Variaveis.java
@@ -0,0 +1,19 @@
+public class Variaveis {
+    public static void main(String[] args){
+        // 2 formas para declarar variavel
+        System.out.println("Aula iniciada!!");
+        int anoNasc = 1980;
+        double peso;
+        peso = 65.7;
+        // esse l e f definem o tipo de dado que a variavel pode armazenar e como vai ser tratado pelo java
+        long populacao = 200000L;
+        float altura = 1.75f;
+        // o f por exemplo informa que queremos float não double
+        char sexo;
+        boolean canhoto;
+
+        // +. -. *, / e % = operadores aritmetricos
+        // !, || e &&; Negação, OU e E
+        System.out.println("Variaveis declaradas!");
+    }  
+}
---
+++ Aulas Ao Vivo(Tercio)/Aula 04/casting.java
@@ -0,0 +1,12 @@
+public class casting {
+    public static void main(String[] args){
+        int numero = 10;
+        double valor = numero;
+
+        System.out.println(valor);
+
+        double decimal = 10.8;
+        int inteiro = (int) decimal;
+        System.out.println(inteiro);
+    }
+}
